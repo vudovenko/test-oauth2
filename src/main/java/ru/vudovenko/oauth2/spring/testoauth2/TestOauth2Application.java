e4d63ddfe5e3ed947588c76c059747be13c64cab
vudovenko <vladislav.udovenko0@yandex.ru>
2024-09-07 12:35:59 +0500

    git init

---
+++ src/main/java/ru/vudovenko/oauth2/spring/testoauth2/TestOauth2Application.java
@@ -0,0 +1,13 @@
+package ru.vudovenko.oauth2.spring.testoauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestOauth2Application.class, args);
+    }
+
+}
