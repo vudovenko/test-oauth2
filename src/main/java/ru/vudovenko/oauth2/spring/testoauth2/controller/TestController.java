039d8deaa0e3e9402486354478aef2a961e02e2b
vudovenko <vladislav.udovenko0@yandex.ru>
2024-09-07 21:03:27 +0500

    возможность получения jwt в методах контроллера

--- src/main/java/ru/vudovenko/oauth2/spring/testoauth2/controller/TestController.java
+++ src/main/java/ru/vudovenko/oauth2/spring/testoauth2/controller/TestController.java
@@ -1,20 +1,29 @@
 package ru.vudovenko.oauth2.spring.testoauth2.controller;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Тестовый контроллер
  */
+@Log4j2
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/delete")
+    @GetMapping("/delete/{id}")
     @PreAuthorize("hasRole('admin')")
-    public String delete() {
+    public String delete(@PathVariable("id") String id,
+                         @AuthenticationPrincipal Jwt jwt) {
+
+        log.info("jwt.getSubject() = {}", jwt.getSubject());
+        log.info("id delete = {}", id);
         return "delete work";
     }
 
